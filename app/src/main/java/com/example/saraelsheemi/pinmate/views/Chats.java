77771a7c3fc65485835af905cfc6079c95e890c2
youssefnabil48 <youssefnabil48@utlook.com>
2018-06-23 14:55:13 +0200

    m

--- app/src/main/java/com/example/saraelsheemi/pinmate/views/Chats.java
+++ app/src/main/java/com/example/saraelsheemi/pinmate/views/Chats.java
@@ -1,17 +1,35 @@
 package com.example.saraelsheemi.pinmate.views;
 
+import android.content.Intent;
+import android.provider.ContactsContract;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.saraelsheemi.pinmate.R;
+import com.example.saraelsheemi.pinmate.controllers.AsynchTaskGet;
+import com.example.saraelsheemi.pinmate.controllers.AsynchTaskPost;
+import com.example.saraelsheemi.pinmate.controllers.Constants;
+import com.example.saraelsheemi.pinmate.controllers.EventListener;
+import com.example.saraelsheemi.pinmate.models.MResponse;
+import com.example.saraelsheemi.pinmate.models.User;
+import com.google.gson.Gson;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class Chats extends Fragment {
 
+    ListView onlineUsersListView;
 
     @Nullable
     @Override
@@ -22,12 +40,30 @@ public class Chats extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        getActivity().setTitle("Chats");
+        getActivity().setTitle("Online Users");
+        init(view);
     }
 
+    private void getOnlineUsers(){
+        AsynchTaskGet asyncGetOnlineUsers = new AsynchTaskGet(getContext(), new EventListener<MResponse>() {
+            @Override
+            public void onSuccess(MResponse object) {
+
+            }
+
+            @Override
+            public void onFailure(Exception e) {
+
+            }
+        });
+    }
     public void init(View view){
-
-
-
+        onlineUsersListView = (ListView) view.findViewById(R.id.onlineUsersContainer);
     }
+
+    private void showMessage(String message) {
+        Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
+    }
+
+
 }
